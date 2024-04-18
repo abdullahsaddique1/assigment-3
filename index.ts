a5e03421dc7e588411846ca78fc23fd627a16130
hafiz abdullah <hafizabdullahsaddique9@gmail.com>
2024-04-18 10:16:12 +0500

    check assigment 3 i will explain

---
+++ index.ts
@@ -0,0 +1,50 @@
+//Declare a variable for storing your favorite movie name with a string type annotation.
+
+const favoriteMovie ="3 IDIOTS";
+
+//Create a template string to introduce yourself, including your name and age.
+
+const age = 22;
+const fullname = "ABDULLAH SADDIQUE";
+console.log(`Hello my name is ${fullname} and i am ${age} years`)
+
+//Initialize a constant with a type annotation for your current mood as a boolean.
+
+const currentmMood: boolean = true
+
+//Combine a user's first and last name using template literals and log the full name.
+const first = 'Abdullah'
+const last = "Saddique"
+console.log ( `${first} ${last}`);
+
+// Create a variable for a user's age with a type annotation and assign a number to it.
+let userAge: number;
+userAge = 22;
+console.log (userAge);
+
+// Use a template string to display a product name and its price.
+const Name = "AC"
+const Price = 50000;
+console.log  (` ${Name} ${Price} `); 
+// Declare a variable to store the number of pets you have with a number type annotation.
+const pets = 0;
+console.log(pets);
+
+// Declare a variable with a boolean type annotation indicating if it is your birthday today.
+const isMyBirthday: boolean = false;
+console.log(isMyBirthday);
+// Create a template string that includes a user's first name, last name, and age with proper labels.
+const userBio = (`first name: ${first} last name: ${last} age:${userAge} `);
+console.log(userBio);
+// Initialize a constant to hold your birth year with a number type annotation.
+const birthYear: number = 2002;
+console.log(birthYear);
+// Write a line of code that uses a template string to combine a city and country into a full address.
+let city: string = "lahore";
+let country: string = "Pakistan";
+console.log (`${city} ${country}`);
+let address = `City:${city} Country: ${country}`;
+console.log
+// Declare a variable for today's temperature with a number type annotation and assign a value to it.
+let temperature: number; temperature = 22;
+console.log(temperature);
